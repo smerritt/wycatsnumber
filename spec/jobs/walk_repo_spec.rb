d8246720bb4d032d7f34f0cb82c2d7d201083e91
Sam Merritt <spam@andcheese.org>
2010-08-19 10:30:48 -0700

    match specs' contents with their names

--- spec/jobs/walk_repo_spec.rb
+++ spec/jobs/walk_repo_spec.rb
@@ -1,66 +1,110 @@
 require File.expand_path('../../spec_helper', __FILE__)
+require 'digest/md5'
 
-describe 'WalkUser#perform' do
+describe 'WalkRepo#perform' do
+
+  before(:each) do
+    @wycats = Author.gen(:github_username => 'wycats', :distance => 0)
+  end
+
+  before(:each) do
+    @repo = 'test/repo'
+  end
 
   before(:each) do
     Resque.stub!(:enqueue)
   end
 
-  before(:each) do
-    @repo_data = {
-      "repositories" => [
+  def repo_data_with_collaborators(collabs)
+    {
+      "contributors" => collabs.map do |collab|
         {
-          "name" => "spiffy-elisp",
-          "has_wiki" => true,
-          "created_at" => "2009/01/05 12:34:56 -0800",
-          "watchers" => 11,
-          "private" => false,
-          "fork" => false,
-          "url" => "http://github.com/smerritt/spiffy-elisp",
-          "pushed_at" => "2010/06/09 13:53:45 -0700",
-          "open_issues" => 0,
-          "has_downloads" => true,
-          "has_issues" => true,
-          "homepage" => "",
-          "forks" => 1,
-          "description" => "Minor modes I use.",
-          "owner" => "smerritt"
-        },
-        {
-          "name" => "rails-2.3.2-app",
-          "has_wiki" => true,
-          "created_at" => "2009/05/21 13:26:58 -0700",
-          "watchers" => 2,
-          "private" => false,
-          "fork" => true,
-          "url" => "http://github.com/smerritt/rails-2.3.2-app",
-          "pushed_at" => "2010/06/21 16:38:55 -0700",
-          "open_issues" => 0,
-          "has_downloads" => true,
-          "has_issues" => false,
-          "homepage" => "",
-          "forks" => 0,
-          "description" => "Clone of rails-2.2.2-app for Rails 2.3.2",
-          "owner" => "smerritt"
-        },
-      ],
+          "login"         => collab,
+          "gravatar_id"   => Digest::MD5.new.hexdigest(collab),
+          "contributions" => rand(10),
+          "type"          => "User",
+        }
+      end
     }
+  end
 
+  def repo_has_collaborators(collabs)
     FakeWeb.register_uri(:get,
-      'http://github.com/api/v2/json/repos/show/smerritt',
-      :body => @repo_data.to_json)
+      "http://github.com/api/v2/json/repos/show/#{@repo}/contributors",
+      :body => repo_data_with_collaborators(collabs).to_json
+      )
   end
 
-  it "enqueues the user's repos for walking" do
-    Resque.should_receive(:enqueue).with(WalkRepo, "smerritt/spiffy-elisp")
 
-    WalkUser.perform('smerritt')
+  it "creates new authors as it sees them" do
+    repo_has_collaborators(%w[alice bob])
+    lambda { WalkRepo.perform(@repo) }.should change(Author, :count).by(2)
   end
 
-  it "ignores forked repos" do
-    Resque.should_not_receive(:enqueue).with(WalkRepo, "smerritt/rails-2.3.2-app")
-    
-    WalkUser.perform('smerritt')
+  it "enqueues new users for walking" do
+    repo_has_collaborators(%w[alice bob])
+    Resque.should_receive(:enqueue).with(WalkUser, 'alice')
+    Resque.should_receive(:enqueue).with(WalkUser, 'bob')
+
+    WalkRepo.perform(@repo)
   end
-  
+
+  it "does not create duplicate authors" do
+    alice = Author.gen(:github_username => 'alice')
+
+    repo_has_collaborators(%w[alice bob])
+    lambda { WalkRepo.perform(@repo) }.should change(Author, :count).by(1)
+  end
+
+  it "does not enqueue existing users for walking" do
+    alice = Author.gen(:github_username => 'alice')
+    repo_has_collaborators(%w[alice bob])
+
+    Resque.should_receive(:enqueue).with(WalkUser, 'bob')
+    Resque.should_not_receive(:enqueue).with(WalkUser, 'alice')
+
+    WalkRepo.perform(@repo)
+  end
+
+  it "creates the project" do
+    repo_has_collaborators(%w[zoidberg])
+    lambda { WalkRepo.perform(@repo) }.should change(Project, :count).by(1)
+  end
+
+  it "does nothing if the project exists" do
+    repo_has_collaborators(%w[zoidberg])
+    project = Project.gen(:name => @repo)
+
+    # this shouldn't happen, but in case it does, don't screw up the DB
+
+    Resque.should_not_receive(:enqueue)
+    lambda do
+      lambda do
+        WalkRepo.perform(@repo)
+      end.should_not change(Project, :count)
+    end.should_not change(Author, :count)
+
+  end
+
+  it "updates distances and paths" do
+    repo_has_collaborators(%w[alice wycats])
+
+    WalkRepo.perform(@repo)
+
+    Author.first(:github_username => 'alice').distance.should == 1
+  end
+
+  it "includes gravatar ids" do
+    repo_has_collaborators(%w[alice])
+
+    WalkRepo.perform(@repo)
+
+    Author.first(:github_username => 'alice').gravatar_id.should ==
+      "6384e2b2184bcbf58eccf10ca7a6563c"   # md5sum of 'alice'
+
+  end
+
+  # this is more of a reminder for me than an actual spec
+  it "wraps the whole thing in a serializable transaction"
+
 end
