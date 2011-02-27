1269366a7b700f8396ffb6b9e19bb6fae38bb003
Samuel Merritt <spam@andcheese.org>
2011-02-27 00:21:44 -0800

    Refactor the config/database.yml parser.
    Some of this can be reused for reading other YAML-format config files.

--- wycatsnumber.rb
+++ wycatsnumber.rb
@@ -3,25 +3,52 @@ require 'bundler'
 Bundler.setup
 Bundler.require
 
-dbinfo = YAML.load(File.read(File.expand_path(File.join(File.dirname(__FILE__), "config", "database.yml"))))[ENV["RACK_ENV"] || 'development']
+module Wycatsnumber
+  def self.database_config_for(env)
+    dbconfig = load_config_yaml("database.yml")
+    if dbconfig
+      dbconfig[env]
+    end
+  end
 
-database_uri = "#{dbinfo['adapter']}://"
+  def self.database_uri
+    env = ENV['RACK_ENV'] || 'development'
+    dbinfo = database_config_for(env)
 
-user = dbinfo['user']
-pass = dbinfo['password']
-host = dbinfo['host']
+    unless dbinfo
+      $stderr.puts "Warning: no database config for #{env} in config/database.yml."
+    end
 
-if user && pass && host
-  database_uri << user << ':' << pass << '@' << host
-elsif user && host
-  database_uri << user << '@' << host
-elsif host
-  database_uri << host
+    database_uri = "#{dbinfo['adapter']}://"
+
+    user = dbinfo['user']
+    pass = dbinfo['password']
+    host = dbinfo['host']
+
+    if user && pass && host
+      database_uri << user << ':' << pass << '@' << host
+    elsif user && host
+      database_uri << user << '@' << host
+    elsif host
+      database_uri << host
+    end
+
+    database_uri << "/" << dbinfo['database']
+
+    database_uri
+  end
+
+  def self.load_config_yaml(filename)
+    config_file = File.expand_path(
+      File.join(File.dirname(__FILE__),
+        "config",
+        filename))
+
+    YAML.load(File.read(config_file)) if File.exist?(config_file)
+  end
 end
 
-database_uri << "/" << dbinfo['database']
-
-DataMapper.setup(:default, database_uri)
+DataMapper.setup(:default, Wycatsnumber.database_uri)
 
 require 'logger'
 Log = ::Logger.new((ENV['RACK_ENV'] || 'development') + '-trace.log')
